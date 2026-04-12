b27d3a3430b4ebc62f76da7936cb996e9c32c902
rcastillo9414 <rcastillom@ucenfotec.ac.cr>
2026-04-12 09:18:45 -0600

    FEAT
    Se crea el sistema de crear,editas y borrar eventos y la logica atras de esto, solicitando la informacion necesaria para crear un evento y los requerimientos necesarios.
    se crear especificaciones para cada rol y cada medida.

--- Server.js
+++ Server.js
@@ -1,32 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./Config/Mongodb");
+const connectDB = require("./config/db");
 
-const authRoutes = require("./Rutas/auth.routes");
-const auth = require("./Middleware/auth");
-const role = require("./Middleware/role");
+const authRoutes = require("./routes/auth.routes");
+const eventRoutes = require("./routes/event.routes");
+const auth = require("./middleware/auth");
+const role = require("./middleware/role");
 
 const app = express();
 
-// Conectar a MongoDB
 connectDB();
 
-// Middlewares globales
 app.use(cors());
 app.use(express.json());
 
-// Ruta principal
 app.get("/", (req, res) => {
   res.json({
     message: "API de Eventos Locales funcionando correctamente"
   });
 });
 
-// Rutas de autenticación
 app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
 
-// Ruta protegida de prueba
+// Ruta privada de prueba
 app.get("/api/private", auth, (req, res) => {
   res.json({
     message: "Ruta privada accesible",
@@ -41,7 +39,7 @@ app.get("/api/moderador", auth, role("moderador"), (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor ejecutándose en el puerto ${PORT}`);
